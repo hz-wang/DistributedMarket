3fd60b440f4720ed75c34c363e107bd504cedb95
pyngon <pyng1101@gmail.com>
2019-10-02 00:38:03 -0700

    add API stub functions

--- dmarket/dmarket/settings.py
+++ dmarket/dmarket/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'dmarket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/dmarket/services/static/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- dmarket/services/urls.py
+++ dmarket/services/urls.py
@@ -4,4 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('user/register/', views.register, name='register'),
+    path('user/login/', views.login, name='login'),
+    path('machine/submit/', views.submit_machine, name='submit_machine'),
+    path('machine/remove/', views.remove_machine, name='remove_machine'),
+    path('machine/list/', views.list_machines, name='list_machine'),
+    path('job/submit/', views.submit_job, name='submit_job'),
+    path('job/cancel/', views.cancel_job, name='cancel_job'),
+    path('job/get_result/', views.get_result, name='get_result'),
+    path('job/get_log/', views.get_log, name='get_log'),
+    path('credit/get/', views.get_credit, name='get_credit'),
 ]
