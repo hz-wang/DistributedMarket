c43c320cfa8373dbc8928217f2e936be421848ec
Haozhe Wang <wanghaozhe1994@hotmail.com>
2019-10-02 00:48:05 -0700

    core model API design

--- dmarket/services/models.py
+++ dmarket/services/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+	# user_id, password, email_address in User
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, default='', blank=True)
+    credit = models.OneToOneField(Credit)
+    followers = models.ManyToManyField(User, related_name='self', blank=True)
+
+    def __str__(self):
+        return self.user
+
+class Machine(models.Model):
+	machine_id = 
+
+
+
+class Job(models.Model):
+
+
+class Metadata(models.Model):
+
+
+class Credit(models.Model):
+	sharing_credit = 
--- dmarket/services/views.py
+++ dmarket/services/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. Distributed Market.")
+
+
