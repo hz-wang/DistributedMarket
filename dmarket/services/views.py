62fcfdf76fe15edf7d1d181cdf56346922dea801
Haozhe Wang <wanghaozhe1994@hotmail.com>
2019-10-05 22:19:43 -0700

    fix bug

--- dmarket/services/views.py
+++ dmarket/services/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 ##### User API #####
+
+def index(request):
+    return HttpResponse("Hello, world. Distributed Market.")
+    
 def register(request):
     context = {}
     context['status'] = True
